1e9818432041792080c502376ea7f257bcd92ffc
Sagnik Chowdhury <sgnkchowdhury@gmail.com>
2018-05-12 14:54:04 -0700

    Added fixed growth logic and animations.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 0. [Test ](https://solarbro.github.io/WebGL_Samples/test/)(from [webgl-examples](https://github.com/mdn/webgl-examples))
 1. [Breakout](https://solarbro.github.io/WebGL_Samples/breakout/)
+2. [Procedural 2D Terrains](https://solarbro.github.io/WebGL_Samples/procedural_terrains_2D/)
+3. [Procedural 2D Trees](https://solarbro.github.io/WebGL_Samples/procedural_trees_2D/)
--- procedural_trees_2D/proctree2d-main.js
+++ procedural_trees_2D/proctree2d-main.js
@@ -19,8 +19,18 @@ const pauseAfterCreation = false;
 const randomColorOnBounce = false;  
 const pauseDuration = 5; //pause for some seconds after each tree is completed
 const rootLocation = new Vec2(canvas.width / 2, canvas.height * 0.9);
+//Terrain options
 const groundSubDivs = 12;
-const heightVariance = canvas.height / 8;
+const heightVariance = canvas.height / 6;
+//Tree options
+const minComplexity = 1;
+const maxComplexity = 10;
+const minDecay = 1;
+const maxDecay = 0.1;
+const minAngleVariance = 0;
+const maxAngleVariance = Math.PI / 2;
+const animationFrames = 60; //number of frames it will take to grow a branch
+var growthAnimation = 0;
 //Game state enum
 const init = 0;
 const load = 1;
@@ -28,15 +38,19 @@ const grow = 2;
 const complete = 3;
 const unload = 4;
 //tree styles
-var angleVariance = Math.PI * Math.random() * 0.5; //+/- 90 degrees max
+var angleVariance = 0;
+//tree growth state
+var branches = [];
 //Scene style
-
+var skyColor = new Color(0, 0, 0);
+var sunColor = new Color(0, 0, 0);
+var moonPhase = 0;
+var numClouds = 0;
 //Game state
 var gameState = unload;
 var processComplete = false;
 
 //Field
-var branches = [];
 //----------
 
 main();
@@ -107,6 +121,24 @@ function drawLeaf(position, direction, size) {
 
 function drawBranch(start, startWidth, end, endWidth) {
 
+}
+
+function drawCurrentBranches() {
+    ctx.beginPath();
+    ctx.strokeStyle = "#000000";
+    //Draw each branch
+    var numBranches = branches.length;
+    for(var i = 0; i < numBranches; ++i) {
+        var branch = branches[i];
+        ctx.moveTo(branch.start.x, branch.start.y);
+        ctx.lineTo(branch.current.x, branch.current.y);
+        branch.current = add(branch.current, branch.animStep);
+    }
+    ctx.stroke();
+    ctx.closePath();
+
+    //Update animation state
+    ++growthAnimation;
 }
 
 function drawRoot() {
@@ -190,6 +222,12 @@ function mul(v, s) {
 }
 
 //utils
+function rotate(v, a) {
+    var cs = Math.cos(a);
+    var sn = Math.sin(a);
+    return new Vec2(v.x * cs - v.y * sn, v.x * sn + v.y * cs);
+}
+
 function getRandInt(start, end) {
     return start + Math.floor(Math.random() * Math.floor(end - start));
 }
@@ -205,6 +243,7 @@ function getRandBranchColor() {
 function getRandLeafColor() {
 
 }
+
 
 function rgbInverse(inColor) {
     return new Color(255 - inColor.r, 255 - inColor.g, 255 - inColor.b);
@@ -260,7 +299,9 @@ function buildGround() {
 
 function genSeeds() {
     //fill out array of branches
-    alert("generating seeds!");
+    //alert("generating seeds!");
+    //TODO: randomize growth params
+    branches[0] = new Branch(rootLocation, add(rootLocation, new Vec2(0, -100)), 3, false);
 }
 
 function genSky() {
@@ -281,8 +322,43 @@ function setSceneOptions() {
 }
 
 function growTree() {
-    alert("growing tree...");
-    processComplete = true;
+    // alert("growing tree...");
+    //Draw active branches
+    drawCurrentBranches();
+    //If done animating
+    if(growthAnimation > animationFrames) {
+        //step growth forward
+        growCurrentBranches();
+        //reset growth animation back to 0 before resuming
+        growthAnimation = 0;
+        //If growth complete, mark as done
+        if(branches.length == 0) {
+            processComplete = true;
+        }
+    }
+}
+
+function growCurrentBranches() {
+    var tmpBranches = [];
+    var numBranches = branches.length;
+    for(var i = 0; i < numBranches; ++i) {
+        var branch = branches[i];
+        if(branch.complexity > 0) {
+            //For now, we always split a branch equally left and right
+            //1
+            var lDir = rotate(branch.animStep, Math.PI / 4);
+            tmpBranches.push(new Branch(branch.current, add(branch.current, mul(lDir, animationFrames)), branch.complexity - 1));
+            //2
+            var rDir = rotate(branch.animStep, -Math.PI / 4);
+            tmpBranches.push(new Branch(branch.current, add(branch.current, mul(rDir, animationFrames)), branch.complexity - 1));
+        }
+    }
+    //Replace branches with this new array
+    branches = [];
+    var numNewBranches = tmpBranches.length;
+    for(var i = 0; i < numNewBranches; ++i) {
+        branches[i] = tmpBranches[i];
+    }
 }
 
 function waitForSceneRefresh() {
@@ -290,4 +366,12 @@ function waitForSceneRefresh() {
     //If auto refresh run timer
     //Check for input state
     processComplete = true;
+}
+
+function Branch(start, end, complexity) {
+    this.start = start;
+    this.current = start;
+    //this.end = end;
+    this.animStep = mul(sub(end, start), 1 / animationFrames);
+    this.complexity = complexity; //complexity = 0 => leaf
 }
